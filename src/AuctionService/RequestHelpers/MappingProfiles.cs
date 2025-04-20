ee5cc6eb1a7ebdd54a58056bb32c5663171facf4
chelsea0111 <xuxinyi9977@gmail.com>
2025-04-20 17:34:02 +1200

    Add DTOs & AutoMapper

---
+++ src/AuctionService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,19 @@
+using AuctionService.DTOs;
+using AuctionService.Entities;
+using AutoMapper;
+
+namespace AuctionService.RequestHelpers;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        // x: source, d: destination
+        // find props from Item to map to AuctionDto
+        CreateMap<Auction, AuctionDto>().IncludeMembers(x => x.Item);
+        CreateMap<Item, AuctionDto>();
+        // 
+        CreateMap<CreateAuctionDto, Auction>().ForMember(d => d.Item, o => o.MapFrom(s => s));
+        CreateMap<CreateAuctionDto, Item>();
+    }
+}
