0884b33b5f45910cbf2b563d2dfdf087d766d3e2
chelsea0111 <xuxinyi9977@gmail.com>
2025-04-24 15:22:09 +1200

    Add MassTransit-RabbitMQ dependencies; Create class library; Create Entity for sending msg

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using MassTransit;
 using MongoDB.Driver;
 using MongoDB.Entities;
 using Polly;
@@ -14,6 +15,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Add HttpClient for SearchService, and set up a retry policy
 builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
+// AddMassTransit() to register the MassTransit services
+builder.Services.AddMassTransit(x => { x.UsingRabbitMq((context, cfg) => { cfg.ConfigureEndpoints(context); }); });
 
 var app = builder.Build();
 
