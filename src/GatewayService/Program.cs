dd8d0d621cf5ba3e0a80b169d7a872870eab32bb
chelsea0111 <xuxinyi9977@gmail.com>
2025-09-11 21:15:19 +1200

    gateway & other services issues

---
+++ src/GatewayService/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer((options) =>
+{
+    options.Authority = builder.Configuration["IdentityServiceUrl"]; // identity service url 
+    options.RequireHttpsMetadata = false; // allow http in prod env
+    options.TokenValidationParameters.ValidateAudience = false;
+    options.TokenValidationParameters.NameClaimType = "username";
+});
+
+var app = builder.Build();
+
+app.MapReverseProxy();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.Run();
